2cf411935e81077a2f9e360c84bd23c28ec4cee9
James <contact@jamesyap.org>
2024-04-21 00:58:58 -0400

    Initial commit

---
+++ docs/01-comp3000/index.md
@@ -0,0 +1,16 @@
+---
+title: COMP 3000
+slug: /comp3000
+---
+
+Taken in **Winter 2024**, taught by **Prof. Lianying Zhao**.
+
+---
+
+## Scope
+
+At the moment, these notes are only on the second-half of the course (midterm onwards). They are also tailored specifically to the [final exam keywords list](./01-keywords.md) with aid from the [final exam review lecture](./01-keywords.md).
+
+## Tutorials
+
+https://homeostasis.scs.carleton.ca/wiki/index.php/COMP3000_Operating_Systems_2024W#Tutorial_Instructions
---
+++ docusaurus.config.ts
@@ -0,0 +1,145 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "James's Wiki 📚",
+  tagline: "Knowledge bank for everything Carleton 🐦‍⬛, and more 🌎",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://docs.jamesyap.org",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+  trailingSlash: false,
+
+  // env vars
+  // https://docusaurus.io/docs/deployment#using-environment-variables
+  customFields: {
+    CONTENTFUL_SPACE_ID: "7m5nzpb6x3ap",
+    CONTENTFUL_ACCESS_TOKEN: "5MxQQuq-Nt3HCxyXGRbERXBwkTzB2PXOo4HLyWOZhz4",
+  },
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "voidranjer", // Usually your GitHub org/user name.
+  projectName: "jamesyap-docs", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/voidranjer/jamesyap-docs/tree/main",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/voidranjer/jamesyap-docs/tree/main",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/james-social-card.png",
+    navbar: {
+      title: "James's Wiki 📚",
+      logo: {
+        alt: "James's logo",
+        src: "img/logo.png",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Notes",
+        },
+        { to: "/blog", label: "Tutorials", position: "left" },
+        { to: "/resources", label: "Resources", position: "left" },
+        {
+          href: "https://jamesyap.org",
+          label: "About Me",
+          position: "right",
+        },
+        {
+          href: "https://github.com/voidranjer",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "About Me",
+          items: [
+            {
+              label: "jamesyap.org",
+              to: "https://jamesyap.org",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "YouTube",
+              href: "https://youtube.com/@ravenjs",
+            },
+            {
+              label: "Instagram",
+              href: "https://www.instagram.com/james_yap8/",
+            },
+            {
+              label: "LinkedIn",
+              href: "https://https://www.linkedin.com/in/james-yapsh/",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Tutorials",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/voidranjer",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} https://jamesyap.org`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
